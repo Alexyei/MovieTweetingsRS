76bfbf41e70bfe8193d10302980d408bcf568193
Alexyei <Aleksyei648@mail.ru>
2023-09-20 21:35:55 +0300

    implicit ratings

--- server/src/builders/calculate_implicit_ratings.ts
+++ server/src/builders/calculate_implicit_ratings.ts
@@ -1,12 +1,82 @@
-import {PrismaClient, TypeEvent} from "@prisma/client";
+import {PrismaClient, RatingType} from "@prisma/client";
+
 const prisma = new PrismaClient();
 
-const w1 = 100;
-const w2 = 50;
-const w3 = 15;
-const w4 = 5;
+// const w1 = 100;
+// const w2 = 50;
+// const w3 = 15;
+// const w4 = 5;
 
-async function getAllUsersInLog() {
+export const eventsParams = {
+    "BUY": {weight: 100},
+    "MORE_DETAILS": {weight: 50, maxCount: 10},
+    "DETAILS": {weight: 15, maxCount: 30},
+    "GENRE_VIEW": {weight: 5, maxCount: 50}
+} as any
+
+//Альтернативный вариант метода
+// async function calculateImplicitRatingsForUser1(userId: number) {
+//     const data = await queryAggregatedLogDataForUser(userId);
+//
+//     const agg_data = {} as any;
+//     let max_rating = 0;
+//
+//
+//     for (const row of data) {
+//         const contentId = (row.movieId || row.genreId) as string;
+//         if (!agg_data[contentId]) {
+//             agg_data[contentId] = {counts: {}, contentType: row.movieId ? 'movie' : 'genre'};
+//         }
+//
+//         agg_data[contentId].counts[row['event']] = row._count._all;
+//     }
+//
+//     const ratings = {} as any;
+//     for (const [contentId, value] of Object.entries(agg_data) as any) {
+//         const counts = value.counts
+//         const rating =
+//             w1 * (counts['BUY'] ?? 0)
+//             + w2 * Math.min((counts['MORE_DETAILS'] ?? 0), 10)
+//             + w3 * Math.min((counts['DETAILS'] ?? 0), 30)
+//             + w4 * Math.min((counts['GENRE_VIEW'] ?? 0), 50);
+//         max_rating = Math.max(max_rating, rating);
+//
+//         ratings[contentId] = {rating, contentType: value.contentType}
+//     }
+//
+//     for (const content_id in ratings) {
+//         ratings[content_id].rating = 10 * ratings[content_id].rating / max_rating;
+//     }
+//
+//     return ratings;
+// }
+
+// async function calculateImplicitRatingsForUser(userId:number) {
+//     const data = await queryAggregatedLogDataForUser(userId);
+//
+//     const weights = {"BUY": 100, "MORE_DETAILS": 50, "DETAILS": 15, "GENRE_VIEW": 5} as any;
+//     const maxCounts = {"BUY": 1, "MORE_DETAILS": 10, "DETAILS": 30, "GENRE_VIEW": 50} as any;
+//     const ratings = {} as any;
+//     let max_rating = 0;
+//
+//     for (const row of data) {
+//         const contentId = (row.movieId || row.genreId) as string | number;
+//         if (!ratings[contentId]) {
+//             ratings[contentId] = {rating: 0, contentId:contentId, contentType: row.movieId ? 'movie' : 'genre'};
+//         }
+//
+//         ratings[contentId].rating+=(weights[row['event']] ?? 0)*Math.min(row._count._all, maxCounts[row['event']] ?? 0)
+//         max_rating = Math.max(max_rating, ratings[contentId].rating);
+//     }
+//
+//     // умножаем на 10 так как рейтинг от 0 до 10
+//     for (const content_id in ratings) {
+//         ratings[content_id].rating = 10 * ratings[content_id].rating / max_rating;
+//     }
+//
+//     return ratings;
+// }
+async function getAllUsersInUserEvents() {
     return prisma.userEvent.findMany({
         distinct: ['userId'],
         select: {
@@ -15,7 +85,7 @@ async function getAllUsersInLog() {
     });
 }
 
-async function queryAggregatedLogDataForUser(userId: number) {
+async function groupedUserEventsForUser(userId: number) {
     return prisma.userEvent.groupBy({
         by: ["userId", "movieId", "genreId", "event"],
         where: {
@@ -25,80 +95,68 @@ async function queryAggregatedLogDataForUser(userId: number) {
     });
 }
 
-//Альтернативный вариант метода
-async function calculateImplicitRatingsForUser1(userId:number) {
-    const data = await queryAggregatedLogDataForUser(userId);
-
-    const agg_data = {} as any;
-    let max_rating = 0;
-
-
-    for (const row of data) {
-        const contentId = (row.movieId || row.genreId) as string;
-        if (!agg_data[contentId]) {
-            agg_data[contentId] = {counts: {}, contentType: row.movieId ? 'movie' : 'genre'};
-        }
-
-        agg_data[contentId].counts[row['event']] = row._count._all;
-    }
-
-    const ratings = {} as any;
-    for (const [contentId, value] of Object.entries(agg_data) as any) {
-        const counts = value.counts
-        const rating =
-            w1 * (counts['BUY'] ?? 0)
-            + w2 * Math.min((counts['MORE_DETAILS'] ?? 0),10)
-            + w3 * Math.min((counts['DETAILS'] ?? 0),30)
-            + w4* Math.min((counts['GENRE_VIEW'] ?? 0),50);
-        max_rating = Math.max(max_rating, rating);
-
-        ratings[contentId] = {rating, contentType: value.contentType}
-    }
-
-    for (const content_id in ratings) {
-        ratings[content_id].rating = 10 * ratings[content_id].rating / max_rating;
-    }
-
-    return ratings;
+export function prepareRatingsForSave(ratings: any[]) {
+    return ratings.filter(rating => rating.movieId).map(rating => {
+        const {genreId, ...rest} = rating
+        return {...rest, rating: Math.round(rating.rating), type: RatingType.IMPLICIT}
+    })
 }
 
-async function calculateImplicitRatingsForUser(userId:number) {
-    const data = await queryAggregatedLogDataForUser(userId);
+export async function saveRatings(ratings: any[]) {
+    return prisma.rating.createMany(
+        {
+            data: ratings
+        })
+}
 
-    const weights = {"BUY": w1, "MORE_DETAILS": w2, "DETAILS": w3, "GENRE_VIEW": w4} as any;
-    const maxCounts = {"BUY": 1, "MORE_DETAILS": 10, "DETAILS": 30, "GENRE_VIEW": 50} as any;
-    const ratings = {} as any;
+async function calculateImplicitRatingsForUser(userId: number) {
+    const data = await groupedUserEventsForUser(userId);
+
+    // const weights = {"BUY": 100, "MORE_DETAILS": 50, "DETAILS": 15, "GENRE_VIEW": 5} as any;
+    // const maxCounts = {"BUY": 1, "MORE_DETAILS": 10, "DETAILS": 30, "GENRE_VIEW": 50} as any;
+
+    const userRatings = [] as { authorId: number, movieId: string | null, genreId: number | null, rating: number }[];
     let max_rating = 0;
 
     for (const row of data) {
-        const contentId = (row.movieId || row.genreId) as string;
-        if (!ratings[contentId]) {
-            ratings[contentId] = {rating: 0, contentType: row.movieId ? 'movie' : 'genre'};
+
+        let rating = userRatings.find(
+            rating => row.movieId != null ? rating.movieId == row.movieId : rating.genreId == row.genreId)
+
+        if (!rating) {
+            rating = {
+                authorId: userId,
+                movieId: row.movieId,
+                genreId: row.genreId,
+                rating: 0
+            }
+            userRatings.push(rating)
         }
 
-        ratings[contentId].rating+=(weights[row['event']] ?? 0)*Math.min(
-            row._count._all,
-                maxCounts[row['event']] ?? 0)
-        max_rating = Math.max(max_rating, ratings[contentId].rating);
+        if (!eventsParams[row['event']]) continue;
 
+        rating.rating +=
+            (eventsParams[row['event']].weight ?? 0)
+            * (eventsParams[row['event']].maxCount ? Math.min(row._count._all, eventsParams[row['event']].maxCount) : row._count._all)
+        max_rating = Math.max(max_rating, rating.rating);
     }
 
     // умножаем на 10 так как рейтинг от 0 до 10
-    for (const content_id in ratings) {
-        ratings[content_id].rating = 10 * ratings[content_id].rating / max_rating;
-    }
+    userRatings.forEach(rating => rating.rating = 10 * rating.rating / max_rating)
 
-    return ratings;
+    return userRatings
 }
 
 async function calculateImplicitRatings() {
-    const users = await getAllUsersInLog();
+    const users = await getAllUsersInUserEvents();
+    let allRatings = [] as any[]
     for (const user of users) {
         const userId = user.userId;
 
         const ratings = await calculateImplicitRatingsForUser(userId)
-        console.log(ratings)
+        allRatings = allRatings.concat(ratings)
     }
+    return allRatings
 }
 
 async function flushDB() {
@@ -109,4 +167,4 @@ async function flushDB() {
     })
 }
 
-flushDB().then(calculateImplicitRatings)
+flushDB().then(calculateImplicitRatings).then(prepareRatingsForSave).then(ratings=>{console.log(ratings);return ratings}).then(saveRatings)
--- server/src/builders/calculate_implicit_ratings_timedecay.ts
+++ server/src/builders/calculate_implicit_ratings_timedecay.ts
@@ -1,21 +1,26 @@
-import {PrismaClient, TypeEvent} from "@prisma/client";
+import {PrismaClient, RatingType, TypeEvent} from "@prisma/client";
+import {eventsParams, prepareRatingsForSave, saveRatings} from "./calculate_implicit_ratings";
 
 
 const prisma = new PrismaClient();
 
-const w1 = 100;
-const w2 = 50;
-const w3 = 15;
-const w4 = 5;
+// const w1 = 100;
+// const w2 = 50;
+// const w3 = 15;
+// const w4 = 5;
 function calculateDecay(ageInDays: number) {
     return 1 / (ageInDays+1);
 }
 
-async function getAllUsersInLog() {
+async function getAllUsersInUserEvents() {
     return prisma.userEvent.findMany({
         distinct: ['userId'],
         select: {
             userId: true
+        },
+        // важно для учёта количества событий, старые не будут учитываться
+        orderBy: {
+            createdAt: 'desc'
         }
     });
 }
@@ -29,76 +34,144 @@ async function getUserEvents(userId: number) {
 }
 
 
+// async function calculateImplicitRatingsForUserWithTimeDecay(userId: number) {
+//     const events = await getUserEvents(userId);
+//     const weights = {"BUY": w1, "MORE_DETAILS": w2, "DETAILS": w3, "GENRE_VIEW": w4} as any;
+//     const maxCounts = {"BUY": 1, "MORE_DETAILS": 10, "DETAILS": 30, "GENRE_VIEW": 50} as any;
+//
+//     const ratings = {} as any;
+//
+//     for (let entry of events) {
+//         const contentId = (entry.movieId || entry.genreId) as string;
+//         const eventType = entry.event;
+//
+//         if (!ratings[contentId]) {
+//             ratings[contentId] = {events: {}, contentType: entry.movieId ? 'movie' : 'genre'}
+//         }
+//
+//         // const ageInYears = Math.floor(
+//         //     (new Date().valueOf() - entry.createdAt.valueOf()) / (1000 * 60 * 60 * 24 * 365.2425)
+//         // );
+//         const ageInDays = Math.floor(
+//             (new Date().valueOf() - entry.createdAt.valueOf()) / (1000 * 60 * 60 * 24)
+//         );
+//
+//         const decay = calculateDecay(ageInDays);
+//
+//         // const decay =randomInt(20);
+//
+//
+//         if (!ratings[contentId].events[eventType]){
+//             ratings[contentId].events[eventType] = {count:0, value: 0}
+//         }
+//         if (ratings[contentId].events[eventType].count < (maxCounts[eventType] ?? 0)){
+//             ratings[contentId].events[eventType].count += 1
+//             ratings[contentId].events[eventType].value += (weights[eventType] ?? 0) * decay;
+//         }
+//     }
+//
+//     let max_rating = 0;
+//     for (const contentId in ratings) {
+//         let  sum = 0
+//         for(const eventType in ratings[contentId].events){
+//             sum+= ratings[contentId].events[eventType].value
+//             max_rating = Math.max(max_rating, sum);
+//         }
+//         ratings[contentId]= { rating: sum, contentType: ratings[contentId].contentType}
+//     }
+//
+//     // умножаем на 10 так как рейтинг от 0 до 10
+//     for (const contentId in ratings) {
+//         ratings[contentId].rating = 10 * ratings[contentId].rating / max_rating;
+//     }
+//
+//     return ratings;
+//
+// }
 async function calculateImplicitRatingsForUserWithTimeDecay(userId: number) {
     const events = await getUserEvents(userId);
-    const weights = {"BUY": w1, "MORE_DETAILS": w2, "DETAILS": w3, "GENRE_VIEW": w4} as any;
-    const maxCounts = {"BUY": 1, "MORE_DETAILS": 10, "DETAILS": 30, "GENRE_VIEW": 50} as any;
 
-    const ratings = {} as any;
+    const userValues = [] as { authorId: number, movieId: string | null, genreId: number | null, value: number, count:number , type: TypeEvent}[];
 
-    for (let entry of events) {
-        const contentId = (entry.movieId || entry.genreId) as string;
-        const eventType = entry.event;
 
-        if (!ratings[contentId]) {
-            ratings[contentId] = {events: {}, contentType: entry.movieId ? 'movie' : 'genre'}
+    for (const row of events) {
+        let rating = userValues.find(
+            rating => rating.type ==
+                row.event && (row.movieId != null ? rating.movieId == row.movieId : rating.genreId == row.genreId))
+
+        if (!rating) {
+            rating = {
+                authorId: userId,
+                movieId: row.movieId,
+                genreId: row.genreId,
+                type: row.event,
+                value: 0,
+                count: 0
+            }
+            userValues.push(rating)
         }
 
-        // const ageInYears = Math.floor(
-        //     (new Date().valueOf() - entry.createdAt.valueOf()) / (1000 * 60 * 60 * 24 * 365.2425)
-        // );
-        const ageInDays = Math.floor(
-            (new Date().valueOf() - entry.createdAt.valueOf()) / (1000 * 60 * 60 * 24)
+        if (!eventsParams[row['event']]) continue;
+
+        // временное затухание по месяцам
+        const ageInMonths = Math.floor(
+            (new Date().valueOf() - row.createdAt.valueOf()) / (1000 * 60 * 60 * 24* 30)
         );
 
-        const decay = calculateDecay(ageInDays);
+        const decay = calculateDecay(ageInMonths);
 
-        // const decay =randomInt(20);
-
-
-        if (!ratings[contentId].events[eventType]){
-            ratings[contentId].events[eventType] = {count:0, value: 0}
+        const maxCount = eventsParams[row['event']].maxCount
+        const weight = eventsParams[row['event']].weight
+        if (weight && maxCount && (rating.count < maxCount)){
+            rating.count += 1
+            rating.value += weight * decay;
         }
-        if (ratings[contentId].events[eventType].count < (maxCounts[eventType] ?? 0)){
-            ratings[contentId].events[eventType].count += 1
-            ratings[contentId].events[eventType].value += (weights[eventType] ?? 0) * decay;
-        }
+
     }
 
+    const userRatings = [] as { authorId: number, movieId: string | null, genreId: number | null, rating: number }[];
     let max_rating = 0;
-    for (const contentId in ratings) {
-        let  sum = 0
-        for(const eventType in ratings[contentId].events){
-            sum+= ratings[contentId].events[eventType].value
-            max_rating = Math.max(max_rating, sum);
+    for (const entry of userValues) {
+        let rating = userRatings.find(
+            rating => entry.movieId != null ? rating.movieId == entry.movieId : rating.genreId == entry.genreId)
+
+        if (!rating) {
+            rating = {
+                authorId: userId,
+                movieId: entry.movieId,
+                genreId: entry.genreId,
+                rating: 0
+            }
+            userRatings.push(rating)
         }
-        ratings[contentId]= { rating: sum, contentType: ratings[contentId].contentType}
+
+        rating.rating+=entry.value
+        max_rating = Math.max(max_rating, rating.rating);
     }
 
     // умножаем на 10 так как рейтинг от 0 до 10
-    for (const contentId in ratings) {
-        ratings[contentId].rating = 10 * ratings[contentId].rating / max_rating;
-    }
+    userRatings.forEach(rating => rating.rating = 10 * rating.rating / max_rating)
 
-    return ratings;
+    return userRatings;
 
 }
-
 async function calculateImplicitRatingsWithTimeDecay() {
-    const users = await getAllUsersInLog();
+    const users = await getAllUsersInUserEvents();
+    let allRatings = [] as any[]
     for (const user of users) {
         const userId = user.userId;
         const ratings = await calculateImplicitRatingsForUserWithTimeDecay(userId);
-        console.log(ratings)
+        allRatings = allRatings.concat(ratings)
     }
+    return allRatings
 }
 
 async function flushDB() {
-    // await prisma.rating.deleteMany({
-    //     where: {
-    //         type: "IMPLICIT"
-    //     }
-    // })
+    await prisma.rating.deleteMany({
+        where: {
+            type: "IMPLICIT"
+        }
+    })
 }
 
-flushDB().then(calculateImplicitRatingsWithTimeDecay)
+flushDB().then(calculateImplicitRatingsWithTimeDecay).then(prepareRatingsForSave).then(ratings=>{console.log(ratings);return ratings}).then(saveRatings)
